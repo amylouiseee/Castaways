9b7b7b6c54eeeece879527ecdaae127866e21b93
Raniyafa <88967704+Raniyafa@users.noreply.github.com>
2021-09-24 14:50:22 +1200

    Added player movement to level 2

---
+++ Castaways/Assets/Scripts/Enviroment/LevelBond.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBond : MonoBehaviour
+{
+    public static float leftSide = -2f;
+    public static float rightSide = 11f;
+    public float internalLeft;
+    public float internalRight;
+
+
+    void Update()
+    {
+        internalLeft = leftSide;
+        internalRight = rightSide;
+    }
+}
+
